aa94c06227794cf27b77b6d4711ca76bf61a87c9
Clinton Manakane <manakanejustin@gmail.com>
2022-09-09 16:05:46 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	int counter = 0;
+	int length_letters = sizeof(letter) / sizeof(char);
+
+	while (counter < length_letters)
+	{
+		putchar(letters[i]);
+		counter = counter + 1;
+	}
+	putchar('\n');
+	return (0);
+}
