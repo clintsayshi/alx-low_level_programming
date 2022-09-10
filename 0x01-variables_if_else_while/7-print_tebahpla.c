ee19b7709d9905387f24379fd7ec16d8c98bb49f
Clinton Manakane <manakanejustin@gmail.com>
2022-09-09 19:55:50 -0700

    reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: print alphabet in lowercase in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	int counter = sizeof(letters) / sizeof(char) - 1;
+
+	while (counter >= 0)
+	{
+		putchar(letters[counter]);
+		counter = counter - 1;
+	}
+	putchar('\n');
+	return (0);
+}
