5fecdfb6c5723ff63bbd5708037694a872bf7ae8
Clinton Manakane <manakanejustin@gmail.com>
2022-09-10 05:14:59 -0700

    combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:  prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		putchar(counter + '0');
+		if (counter < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		counter = counter + 1;
+	}
+	return (0);
+}
