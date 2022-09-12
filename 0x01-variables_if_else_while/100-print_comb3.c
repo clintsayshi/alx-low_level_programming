2b63dd3369ebd126dddf82fc58ce9a00ba4e5731
Clinton Manakane <manakanejustin@gmail.com>
2022-09-12 06:17:58 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: print all single base 10 numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = b + 1; b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+
+			if (a + b != 8 + 9)
+			{
+				putchar(' ');
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
