661c4a45eddd33fe7d248bf0ac527bf7d902e6aa
Clinton Manakane <manakanejustin@gmail.com>
2022-09-13 10:51:21 -0700

    added print_alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,8 @@
  * Main header file for function prototypes
  *
  */
-
+/* function to print a char */
 int _putchar(int character);
+
+/* function to print alphabet, in lowercase, followed by a new line */
+void print_alphabet(void);
