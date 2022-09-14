115c76a08934d3f17a50644c81551dad1fe966b8
Clinton Manakane <manakanejustin@gmail.com>
2022-09-14 09:07:03 -0700

    added abs and print last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,5 +21,11 @@ int _isalpha(int c);
 /* function that prints the sign of a number */
 int print_sign(int n);
 
+/* function that computes the absolute value of an integer */
+int _abs(int);
+
 /* function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59 */
 void jack_bauer(void);
+
+/* function that prints the last digit of a number */
+int print_last_digit(int);
