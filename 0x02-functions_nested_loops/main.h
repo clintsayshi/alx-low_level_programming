b9e6e9d468b5a70646d922b43768992559a0087e
Clinton Manakane <manakanejustin@gmail.com>
2022-09-14 09:00:48 -0700

    added 24 hours function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,6 @@ int _isalpha(int c);
 
 /* function that prints the sign of a number */
 int print_sign(int n);
+
+/* function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59 */
+void jack_bauer(void);
