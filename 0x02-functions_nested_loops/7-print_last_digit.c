0146c26d4aa418bf5dea73ad97bfd78ca1ae2feb
Clinton Manakane <manakanejustin@gmail.com>
2022-09-14 09:27:50 -0700

    added abs()

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * print_last_digit - Last digit
@@ -9,6 +10,6 @@
 
 int print_last_digit(int n)
 {
-	_putchar(n % 10 + '0');
-	return (n % 10);
+	_putchar(abs(n) % 10 + '0');
+	return (abs(n) % 10);
 }
