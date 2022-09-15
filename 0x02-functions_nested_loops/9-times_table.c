d0b883e76b0c328258115927d76918241b5cc0c5
Clinton Manakane <manakanejustin@gmail.com>
2022-09-15 08:04:48 -0700

    removed js syntax

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * times_table - print times table
+ * Description:  prints the n times table, starting with 0
+ * Return: Void
+ */
+void times_table(void)
+{
+	int a, b, counter;
+
+	for (a = 0; a <= 9; a++)
+	{
+		counter = 0;
+		for (let b = 0; b <= 9; b++)
+		{
+			printf("%d", counter);
+			if (counter != 9)
+			{
+				printf(", ");
+			}
+			counter = counter + a;
+		}
+		printf("\n");
+	}
+}
