3679ab96891fc9c9a392e9adec47f1676968bd82
Clinton Manakane <manakanejustin@gmail.com>
2022-09-15 04:25:42 -0700

    updated 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,23 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		printf("%d", i);
-		if (i < 98)
-			printf(", ");
-		else
-			putchar('\n');
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
 	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	_putchar('\n');
 }
