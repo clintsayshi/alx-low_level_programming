a124a551010083f6eddaf6df6cc60cbe5e74294c
Clinton Manakane <manakanejustin@gmail.com>
2022-09-15 08:05:51 -0700

    removed js let

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 		for (a = 0; a <= n; a++)
 		{
 			counter = 0;
-			for (let b = 0; b <= n; b++)
+			for (b = 0; b <= n; b++)
 			{
 				printf("%d", counter);
 				if (counter != n)
