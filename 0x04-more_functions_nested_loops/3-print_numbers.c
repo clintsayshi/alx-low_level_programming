273dc2cf83b14dc48a8f6af95e8394c71c41e0bb
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 15:21:37 -0700

    added print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Print numbers
+ * Description: prints the numbers, from 0 to 9, followed by a new line
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+}
