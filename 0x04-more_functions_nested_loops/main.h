523ecd54530bdee4d2b1a178cdf77d37e37def43
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 16:10:16 -0700

    added print_diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -21,6 +21,8 @@ void more_numbers(void);
 /* function that draws a straight line in the terminal. */
 void print_line(int n);
 
+/* function that draws a diagonal line on the terminal */
+void print_diagonal(int n);
+
 /* function to print a char */
 int _putchar(int character);
-
