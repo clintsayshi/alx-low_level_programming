da2054915627a022225814b6e0950f935404aa95
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 15:09:55 -0700

    function multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * mul - Multiply 2 numbers
+ * @a: 1st integer value
+ * @b: 2nd integer value
+ * Description: multiplies two integers
+ * Return: product of a and b
+ */
+
+int (int a, int b)
+{
+	return (a * b);
+}
