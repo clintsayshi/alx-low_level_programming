34ee13b8b0308ea049dca36f0cc501ad8324e517
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 15:16:51 -0700

    added most functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,16 @@ int _isdigit(int c);
 /* function that multiplies two integers */
 int mul(int a, int b);
 
+/* function that prints the numbers, from 0 to 9, followed by a new line */
+void print_numbers(void);
+
+/* function that prints the numbers, from 0 to 9, followed by a new line */
+void print_most_numbers(void);
+
+/* function that prints 10 times the numbers, from 0 to 14, followed by a new line */
+void more_numbers(void);
+
+/* function that draws a straight line in the terminal. */
+void print_line(int n);
+
 
