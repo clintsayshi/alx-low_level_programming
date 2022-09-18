4fef3396de9dec5b189ead555fa40e5b06e341de
Clinton Manakane <manakanejustin@gmail.com>
2022-09-18 02:48:27 -0700

    first commid

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - Prints a number
+ * @n: an integer value
+ * Decription: function that prints an integer
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	int a = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a = -a;
+	}
+
+	if ((a / 10) > 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
