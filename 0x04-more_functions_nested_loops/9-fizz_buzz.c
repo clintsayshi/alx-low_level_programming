f05803d52b8c9fbbc4061251d2cc78c059475234
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 18:09:55 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - FizzBuzz
+ *
+ * Return: Void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		putchar(' ');
+	}
+
+	printf("\n");
+
+	return (0);
+}
