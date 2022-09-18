0223647a5bb1cf4e78d44ac615ddf6c40d2593ae
Clinton Manakane <manakanejustin@gmail.com>
2022-09-17 19:01:00 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - Print Triangle
+ * @size: integer value
+ * Description: prints a triangle, followed by a new line
+ * Return: Void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int temp;
+
+	for (i = 1; i <= size; i++)
+	{
+		temp = size - i;
+		for (j = 0; j < temp; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < size - temp; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
