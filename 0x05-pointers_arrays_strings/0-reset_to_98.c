18c55effcc002d12a52a330311e586951cf1fdaa
Clinton Manakane <manakanejustin@gmail.com>
2022-09-20 16:33:23 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Changes int value
+ * @n: pointer to an integer variable
+ * Description: takes a n and updates the value it points to
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
