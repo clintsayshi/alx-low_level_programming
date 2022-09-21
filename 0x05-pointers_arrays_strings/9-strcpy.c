5c3f22079c724280fe59c22ba7facabe3353bd14
Clinton Manakane <manakanejustin@gmail.com>
2022-09-21 01:29:24 -0700

    another betty fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,8 @@
  * *_strcpy - copies string pointed to
  * @dest: destination string
  * @src: source string
- * Description: copies the string pointed to by src, including the terminating null
+ * Description: copies the string pointed to by
+ * src, including the terminating null
  * byte (\0), to the buffer pointed to by dest
  * Return: pointer to dest
  */
