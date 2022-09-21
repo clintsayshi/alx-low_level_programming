5e653f978c2017050822f7004beb21dea32b89a0
Clinton Manakane <manakanejustin@gmail.com>
2022-09-20 22:56:04 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - print string
+ * @str: pointer to string variable
+ * Description: prints a string, followed by \n to stdout
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n);
+}
