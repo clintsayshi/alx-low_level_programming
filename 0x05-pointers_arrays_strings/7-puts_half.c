b869c66d98638984f328dff3c8cebe7690c2ee87
Clinton Manakane <manakanejustin@gmail.com>
2022-09-21 00:38:14 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * puts_half - print half of a string
+ * @str: pointer to string variable
+ * Description: print half of str, followed by \n
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	unsigned int i;
+	unsigned int start;
+
+	if (strlen(str) % 2 == 0)
+		start = strlen(str) / 2;
+	else
+		start = (strlen(str) - 1) / 2;
+
+	for (i = start; i < strlen(str); i++)
+	{
+		_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
