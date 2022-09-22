937e03fc66dda77146d414e806d16da5459c6373
Clinton Manakane <manakanejustin@gmail.com>
2022-09-22 05:40:08 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * reverse_array - reverse array
+ * @a: int array
+ * @n: length of the array
+ * Description: reverses the content of an array of integers
+ * Return: reversed int array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *temp = (int *)malloc(n * sizeof(int));
+	int i;
+
+	for (i = n; i >= 0; i--)
+	{
+		temp[n - i] = a[i - 1];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		a[i] = temp[i];
+	}
+	free(temp);
+
+	return (a);
+}
