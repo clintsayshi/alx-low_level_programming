8d20619600220c6263ad95f3a24435773fac1893
Clinton Manakane <manakanejustin@gmail.com>
2022-09-27 20:11:07 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: source string
+ * @c: character to locate in @s
+ *
+ * Return: character matching @c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
