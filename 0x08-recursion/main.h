108a7b259ccb707f43705d9cf8cf25670df7f18f
Clinton Manakane <manakanejustin@gmail.com>
2022-09-28 16:21:42 -0700

    added main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,29 @@
+/* putchar */
+int _putchar(int c);
+
+/* function that prints a string, followed by a new line */
+void _puts_recursion(char *s);
+
+/* function that prints a string in reverse */
+void _print_rev_recursion(char *s);
+
+/* function that returns the length of a string */
+int _strlen_recursion(char *s);
+
+/* function that returns the factorial of a given number */
+int factorial(int n);
+
+/* function that returns the value of x raised to the power of y */
+int _pow_recursion(int x, int y);
+
+/* function that returns the natural square root of a number */
+int _sqrt_recursion(int n);
+
+/* function that returns 1 if the input integer is a prime number, otherwise return 0 */
+int is_prime_number(int n);
+
+/* function that returns 1 if a string is a palindrome and 0 if not */
+int is_palindrome(char *s);
+
+/* function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0 */
+int wildcmp(char *s1, char *s2);
