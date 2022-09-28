56cc88c042187e57698ec27600790e6e879b1476
Clinton Manakane <manakanejustin@gmail.com>
2022-09-27 20:48:47 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+
+/**
+ * *_strpbrk - search string for any set of bytes
+ * @s: string to be searched
+ * @accept: bytes to search in @s
+ *
+ * Return: pointer to first byte found or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+		i++;
+	}
+
+	return (NULL);
+}
