dc70099d9292d379ffc56623c4923c87df2c6894
Clinton Manakane <manakanejustin@gmail.com>
2022-09-30 12:21:38 -0700

    fix:unused variable argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ *
+ * Return: Always 0
+ */
 
-int main(int argc, char* argv[])
+int main(int argc, __attribute__((unused)) char* argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
