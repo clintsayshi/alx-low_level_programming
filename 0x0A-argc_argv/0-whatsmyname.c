02d1ff4aaed84222f868f4a730ac992dba02cf21
Clinton Manakane <manakanejustin@gmail.com>
2022-09-30 12:53:51 -0700

    fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: Always 0
  */
 
-int main(int argc, __attribute__((unused)) char* argv[])
+int main(__attribute__((unused)) int argc, char* argv[])
 {
-	printf("%d\n", argc - 1);
+	printf("%s\n", argv[0]);
 	return (0);
 }
