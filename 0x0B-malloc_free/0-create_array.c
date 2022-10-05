6fa0a511b04cc6b52c4954287f4c88f71ee9c710
Clinton Manakane <manakanejustin@gmail.com>
2022-10-05 06:22:30 -0700

    fix:check if array i snull

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	unsigned int i;
 
-	if (size == 0)
+	if (size == 0 || array == NULL)
 		return (NULL);
 	i = 0;
 	while (i < size)
