b567bb08e079d53ae089a0144fc222df463681ca
Clinton Manakane <manakanejustin@gmail.com>
2022-10-13 17:57:20 -0700

    fix:accounted for string being NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,11 +17,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(args, char*));
+		if (va_arg(args, char*) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(args, char*));
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 
 	}
 	va_end(args);
-	putchar('\n');
+	putchar("\n");
 }
