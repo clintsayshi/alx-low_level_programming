d8b9cb9e779a8095f0f4c42e2c459a212eeb217e
Clinton Manakane <manakanejustin@gmail.com>
2022-10-13 17:34:53 -0700

    firsr commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings
+ * @separator: separates the printed strings
+ * @n: number of arguments passed to function
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(args, char*));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+
+	}
+	putchar('\n');
+	va_end(args);
+}
