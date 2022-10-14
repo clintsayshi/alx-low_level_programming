af76342ca7d8b73c202dbe7e49fb5b9ef19057c4
Clinton Manakane <manakanejustin@gmail.com>
2022-10-13 18:02:32 -0700

    fiex:syntax error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,19 +12,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *temp;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char*) == NULL)
+		temp = va_arg(args, char*);
+
+		if (temp == NULL)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(args, char*));
+			printf("%s", temp);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 
 	}
 	va_end(args);
-	putchar("\n");
+	putchar('\n');
 }
